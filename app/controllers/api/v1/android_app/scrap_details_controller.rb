507f1b9c87c7a8fcaaa290a712ef0884b0a960d9
Ajinkya <ajinkyakathe@yahoo.in>
2016-11-24 09:59:55 +0530

    minor change

--- app/controllers/api/v1/android_app/scrap_details_controller.rb
+++ app/controllers/api/v1/android_app/scrap_details_controller.rb
@@ -3,12 +3,16 @@ class Api::V1::AndroidApp::ScrapDetailsController < BaseController
   skip_before_filter :verify_authenticity_token
 
   def save_details
-    if params[:lat].present?
+    if params.present?
       # params[:array].each do |record|
       #   data = JSON.parse(record)
       #   puts data
       # end
-      BankDetail.create(pincode: params[:pincode], latitude: params[:latitude], longitude: params[:longitude])
+      params[:array].each do |record|
+        BankDetail.create(pincode: params[:pincode], latitude: params[:latitude], longitude: params[:longitude],
+                          status: record['status'], bank_name: record['bank_name'], address: record['address'])
+      end
+
       render json: { response: 'Success' }
     end
   end
