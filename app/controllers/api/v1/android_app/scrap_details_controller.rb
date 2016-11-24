017faa6b61b80afe4aecb8cf15a6f25e83ff2776
Ajinkya <ajinkyakathe@yahoo.in>
2016-11-24 10:28:03 +0530

    adding validation

--- app/controllers/api/v1/android_app/scrap_details_controller.rb
+++ app/controllers/api/v1/android_app/scrap_details_controller.rb
@@ -4,16 +4,17 @@ class Api::V1::AndroidApp::ScrapDetailsController < BaseController
 
   def save_details
     if params.present?
-      # params[:array].each do |record|
-      #   data = JSON.parse(record)
-      #   puts data
-      # end
-      params[:array].each do |record|
-        BankDetail.create(pincode: params[:pincode], latitude: params[:latitude], longitude: params[:longitude],
-                          status: record['status'], bank_name: record['bank_name'], address: record['address'])
+      if BankDetail.select(:id).where(pincode: params[:pincode]).nil?
+        params[:array].each do |record|
+          BankDetail.create(pincode: params[:pincode], latitude: params[:latitude], longitude: params[:longitude],
+                            status: record['status'], bank_name: record['bank_name'], address: record['address'])
+        end
+        render json: { response: 'Success' }
+      else
+        render json: { response: 'Pin already present' }
       end
-
-      render json: { response: 'Success' }
+    else
+      render json: { response: 'Wrong parameters' }
     end
   end
 
